13a35d4ca376fd1c01dd151f9904218e3b5a9ebb
rk <ronald.koenig@gematik.de>
2023-07-21 14:59:27 +0200

    chore: add trusted issuer

---
+++ app/src/main/java/de/gematik/security/mobileverifier/Settings.kt
@@ -0,0 +1,8 @@
+package de.gematik.security.mobileverifier
+
+import java.net.URI
+
+object Settings {
+    val trustedIssuer = URI.create("did:key:xzUC78bhyjquwftxL92uP5xdUA7D7rtNQ43LZjvymncP2KTXtQud1g9JH4LYqoXZ6fyiuDJ2PdkNU9j6cuK1dsGjFB2tEMvTnnHP7iZJomBmmY1xsxBqbPsCMtH6YmjP4ocfGLwv")
+}
+
